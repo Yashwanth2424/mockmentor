f04f9409c32d20dde65e440f5ae158adac265f33
Yashwanth <thalkayashwanth084@gmail.com>
2026-05-31 10:01:37 +0200

    fix build hang - next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
+  },
 };
 
 export default nextConfig;
